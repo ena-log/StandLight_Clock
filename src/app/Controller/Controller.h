589bc207709236038e01693377d17aacd4ce0cf9
banginhwa <circle87088@gmail.com>
2022-09-14 05:13:18 +0000

    setting

---
+++ src/app/Controller/Controller.h
@@ -0,0 +1,20 @@
+#ifndef __CONTROLLER_H__
+#define __CONTROLLER_H__
+
+#include <string>
+#include "View.h"
+#include "Service.h"
+
+class Controller
+{
+private:
+    int lightState;
+    Service *service;
+
+public:
+    Controller(Service *service);
+    virtual ~Controller();
+    void updateEvent(std::string strBtn);
+};
+
+#endif /* __CONTROLLER_H__ */
---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <wiringPi.h>
+#include "Button.h"
+#include "Led.h"
+#include "Listener.h"
+#include "Controller.h"
+#include "View.h"
+#include "Service.h"
+
+// func. main
+// 1. 프로그램의 시작점을 알려줌
+// 2. 초기화 및 로딩
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    
+    Button button1(27);
+    Led led1(21);
+    Led led2(22);
+    Led led3(23);
+    Led led4(24);
+    Led led5(25);
+    View view(&led1, &led2, &led3, &led4, &led5);
+    Service service(&view);
+    Controller control(&service);
+    Listener listener(&button1, &control);
+    
+    while (1)
+    {
+        listener.checkEvent();
+        view.lightView();
+        delay(50);
+    }
+
+    return 0;
+}
